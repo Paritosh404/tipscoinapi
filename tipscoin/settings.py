18b911a94f11412d564ec6555a178ff5d9a64f01
Paritosh404 <54989538+Paritosh404@users.noreply.github.com>
2021-05-22 11:47:01 +0530

    Update settings.py

--- tipscoin/settings.py
+++ tipscoin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -159,3 +159,4 @@ import dj_database_url
 
 prod_db=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())

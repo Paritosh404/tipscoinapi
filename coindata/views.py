5460e14620956afee80ccd883799711cd3a97885
Paritosh404 <54989538+Paritosh404@users.noreply.github.com>
2021-05-24 01:15:23 +0530

    error-resolve

--- coindata/views.py
+++ coindata/views.py
@@ -175,7 +175,7 @@ def alert_check():
 scheduler = BackgroundScheduler()
 scheduler.add_job(get_crypto_update, "interval", seconds=136, id='currency001', replace_existing=True)
 scheduler.start()
-get_crypto_update()
+#get_crypto_update()
 
 class CurrencyView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     queryset = CurrencyData.objects.all().order_by('rank')

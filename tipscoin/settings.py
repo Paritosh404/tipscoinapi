692360cdfa738784b00800a5152881b01cb56d8a
Paritosh404 <54989538+Paritosh404@users.noreply.github.com>
2021-05-23 11:38:24 +0530

    Templates Update

--- tipscoin/settings.py
+++ tipscoin/settings.py
@@ -93,7 +93,7 @@ crypto_api2 = os.environ['crypto_api2']
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

1468889cfefeb1674d284d37224b7afd840451e6
Paritosh404 <54989538+Paritosh404@users.noreply.github.com>
2021-05-29 19:00:25 +0530

    UP

--- tipscoin/middleware.py
+++ tipscoin/middleware.py
@@ -4,9 +4,11 @@ from django.utils.deprecation import MiddlewareMixin
 class RejectSpambotRequestsMiddleware(MiddlewareMixin):
 
     def process_request(self, request): 
+        whitelist = ['tipcoinapi.herokuapp.com', 'tipscoinapi.herokuapp.com']
         referer = request.headers['Referer']
         print(referer)
-        if referer == 'tipcoinapi.herokuapp.com' or referer == 'tipscoinapi.herokuapp.com':
-            return  # reject the request and return 403 forbidden response
+        for i in whitelist:
+            if i in referer:
+                return  # reject the request and return 403 forbidden response
 
         return HttpResponseForbidden() # return None in case of a valid request
